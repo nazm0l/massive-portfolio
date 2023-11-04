6c339a48ede8f7a2b710ebb889d305677a033fe9
nazm0l <tanjirahmednazmul@gmail.com>
2023-11-05 01:44:18 +0600

    Login with google implemented

--- src/components/LoginProvider.js
+++ src/components/LoginProvider.js
@@ -1,23 +1,24 @@
 "use client";
+import { signIn } from "next-auth/react";
+import Image from "next/image";
 
 const LoginProvider = () => {
   return (
     <div>
-      <div>
-        {/* login with google with icon */}
+      <div className="w-full">
         <button
-          onClick={() => alert("Logged in clicked")}
-          className="bg-[#ff1e1e] text-white p-3 rounded-md mr-2"
+          onClick={() => signIn("google", { callbackUrl: "/" })}
+          className="bg-gradient-to-r from-[#f39d99] to-[#ff1e1ec5] text-white p-3 rounded-md flex items-center gap-2"
         >
+          <Image src="/google.svg" alt="google" width={30} height={30} />
           Login with Google
         </button>
-        {/* login with github */}
-        <button
+        {/* <button
           onClick={() => alert("Github in clicked")}
           className="bg-[#ff1e1e] text-white p-3 rounded-md"
         >
           Login with Github
-        </button>
+        </button> */}
       </div>
     </div>
   );
